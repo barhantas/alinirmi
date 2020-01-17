04998dd3ce5063ec4e1ea43e86fe277213bae528
Barış Hantaş <baris.hantas@iyzi-sfd-48.local>
2020-01-17 16:29:56 +0300

    Test funny random xD

--- decision.js
+++ decision.js
@@ -1,5 +1,14 @@
 function decision() {
   console.log("AHOY ! I am gonna decide to what you should do !");
+  var shouldBuyText;
+  if (Math.random() > 0.5) {
+    shouldBuyText = "BUY";
+  } else {
+    shouldBuyText = "SELL";
+  }
+
+  var myDiv = document.getElementById("what-i-sould-do");
+  myDiv.innerHTML = "You sould " + shouldBuyText + " the currency($)";
 }
 
 decision();
